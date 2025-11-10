49f0e86b3559c9d22a8298a18d6cf0cff76a3ee8
Elijah Mawuli Amadi <elijahmawuliamadi@Elijahs-MacBook-Pro.local>
2025-11-10 22:18:51 +0000

    made changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>Hello World</h2>
+        <h2>Hey World</h2>
         <p>
           Welcome Buddy to Amplify Deployment!!!
         </p>
