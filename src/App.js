876931e5e009bb3df38ddf2cb30a94ed8b59ab7d
Elijah Mawuli Amadi <elijahmawuliamadi@Elijahs-MacBook-Pro.local>
2025-11-11 15:59:50 +0000

    testing approvaal

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome World</h2>
+        <h2>Hey World</h2>
         <p>
-          Welcome Buddy to Amplify Deployment!!!
+          New Buddy to Amplify Deployment!!!
         </p>
       </header>
     </div>
